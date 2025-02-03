2278439812fab02ba5c763047a1fca9cce152ddf
Rafael <rafael@multitecsistemas.com.br>
2025-02-03 15:13:13 -0300

    SCF1002

---
+++ kotlin/modules/scf/src/main/kotlin/info/agilite/scf/tasks/Scf1002CrudService.kt
@@ -0,0 +1,17 @@
+package info.agilite.scf.tasks
+
+import info.agilite.boot.crud.AgiliteCrudRepository
+import info.agilite.boot.crud.DefaultSduiCrudService
+import info.agilite.boot.metadata.models.EntityMetadata
+import info.agilite.core.extensions.splitToList
+import info.agilite.shared.entities.cgs.Cgs80
+import org.springframework.stereotype.Component
+
+@Component
+class Scf1002CrudService(crudRepository: AgiliteCrudRepository) : DefaultSduiCrudService<Cgs80>(crudRepository) {
+  override fun getColumnQueriesToList(taskName: String, entityMetadata: EntityMetadata): List<String> {
+    return "scf02dtVenc, scf02lancamento.scf11data|Data Pagamento, scf02tipo, scf02entidade.cgs80nome, scf02entidade.cgs80ni, scf02valor, scf02forma.cgs38nome".splitToList();
+  }
+
+
+}
